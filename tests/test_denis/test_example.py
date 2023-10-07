05b546ae9aaa79c62038b28ee542677917f76b15
Denis Tadzhibaev <109044090+DionisIno@users.noreply.github.com>
2023-10-07 19:31:41 +0300

    refactor: modify test_example.py (#4)

--- tests/test_denis/test_example.py
+++ tests/test_denis/test_example.py
@@ -4,8 +4,8 @@ import requests
 def test_example():
     url = "https://serverest.dev/login"
     data = {
-      "email": "fulano@qa.com",
-      "password": "teste"
+      "email": "denisbacchus@qa.com",
+      "password": "qwerty123456zxcvb"
 }
     response = requests.post(url, json=data)
     print(response.json())
