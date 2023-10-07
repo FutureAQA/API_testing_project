d1a24b824b1ec76334d7f8c47164a06350f69ed0
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-10-07 23:37:40 +0400

    add: test_get_pets_by_status_code

--- tests/test_nata/test_pets.py
+++ tests/test_nata/test_pets.py
@@ -5,3 +5,4 @@ def test_get_pets_by_status_code():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
     response = requests.get(url=url)
     print(response.status_code)
+    assert response.status_code == 200, "Wrong status code"
