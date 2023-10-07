9461116666cf0912764cf216089abca17684d349
Elena <peregovorun@gmail.com>
2023-10-07 21:42:46 +0300

    feat: add test_pets.py

--- tests/test_alena/test_pets.py
+++ tests/test_alena/test_pets.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def test_get_pets_by_status():
+def test_get_pets_by_status_pending():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
     response = requests.get(url=url)
     print(response.json())
