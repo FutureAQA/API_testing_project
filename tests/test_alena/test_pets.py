b943fc627a6d71ba3cf18b55b27ae23a0b89afaf
Elena <peregovorun@gmail.com>
2023-10-07 23:23:59 +0300

    feat: ref test_pets.py

--- tests/test_alena/test_pets.py
+++ tests/test_alena/test_pets.py
@@ -5,3 +5,9 @@ def test_get_pets_by_status_pending():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
     response = requests.get(url=url)
     print(response.json())
+
+
+def test_get_pets_by_status_sold():
+    url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=sold'
+    response = requests.get(url=url)
+    print(response.json())
