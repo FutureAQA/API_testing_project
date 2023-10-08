865dd78ca5b11a5b8e4e6089507e3ae029316b70
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2023-10-08 15:47:10 +0400

    add: test_get_pets_headers (#9)

--- tests/test_nata/test_pets.py
+++ tests/test_nata/test_pets.py
@@ -4,5 +4,10 @@ import requests
 def test_get_pets_by_status_code():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
     response = requests.get(url=url)
-    print(response.status_code)
     assert response.status_code == 200, "Wrong status code"
+
+
+def test_get_pets_headers():
+    url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
+    response = requests.get(url=url)
+    print(response.headers)
