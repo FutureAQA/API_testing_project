0d13d0f904aaeb3b7b5ef1e40114f1d4030c9335
OlRules <vm92@yandex.ru>
2023-10-09 17:51:34 +0500

    feat: add test_example.py

--- tests/test_olga_r/test_example.py
+++ tests/test_olga_r/test_example.py
@@ -1,11 +1,11 @@
 import requests
 
-def test_get_by_statu_available():
+def test_get_by_status_available():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=available'
     response = requests.get(url=url)
     print(response.json())
 
-def test_get_by_statu_pending():
+def test_get_by_status_pending():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
     response = requests.get(url=url)
     print(response.json())
