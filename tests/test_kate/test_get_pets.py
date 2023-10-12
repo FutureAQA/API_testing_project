8a7a20f86e057cbfc53d497ddf080e848f6460b0
Ekaterina Zotova <ekatzotova@gmail.com>
2023-10-12 07:57:24 +0300

    feat: add test_get_pets_by_status add data_kate.py

--- tests/test_kate/test_get_pets.py
+++ tests/test_kate/test_get_pets.py
@@ -1,7 +1,17 @@
 import requests
+import pytest
+from data.urls import Urls
+from tests.test_kate.data_kate import AllData
 
 
 def test_get_pets_is_json():
     url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=sold'
     response = requests.get(url=url)
     assert 'application/json' in response.headers.get('Content-Type', '')
+
+
+@pytest.mark.parametrize("status", AllData.pets_status)
+def test_get_pets_by_status(status):
+    url = f'/pet/findByStatus?status={status}'
+    response = requests.get(f'{Urls.BASE_URL}{url}')
+    assert response.status_code == 200
