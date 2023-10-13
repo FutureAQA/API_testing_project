ead1fcd2de99a71f7e37946c97d4194e7eed5398
Galina Danilova <galya.danilova@gmail.com>
2023-10-13 16:42:48 -0400

    add few example tests (#21)

---
+++ tests/test_galya/test_example.py
@@ -0,0 +1,36 @@
+import os
+
+import pytest
+import requests
+
+
+def test_get_find_pets_by_status_pending():
+    url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending'
+    response = requests.get(url=url)
+    print(response.json())
+
+
+def test_get_find_pets_by_status_sold():
+    url = 'https://petstore.swagger.io/v2/pet/findByStatus?status=sold'
+    response = requests.get(url=url)
+    print(response.json())
+
+
+def test_get_find_pets_by_id():
+    url = 'https://petstore.swagger.io/v2/pet/12'
+    response = requests.get(url=url)
+    assert response.status_code == 200, "Wrong status code"
+    print(response.json())
+
+
+def test_get_user_by_user_name():
+    url = 'https://petstore.swagger.io/v2/user/Polkan'
+    response = requests.get(url=url)
+    print(response.json())
+
+
+def test_get_purchase_by_id():
+    url = 'https://petstore.swagger.io/v2/store/order/9'
+    response = requests.get(url=url)
+    assert response.status_code == 404, "Wrong status code"
+    print(response.json())
