8dcbdfe5d66ff4a308eebdd0aeac8b893215f3ff
verafes <verafes@gmail.com>
2023-10-13 17:28:01 -0700

    Add: get_pet_by_id tests, data_pets, conf.py

--- tests/test_vera/test_pets_get.py
+++ tests/test_vera/test_pets_get.py
@@ -1,12 +1,13 @@
 """Test cases for validating pets data and API responses using the GET method."""
-
 import pytest
 import requests
+from pprint import pprint
 
 from tests.test_vera.data.status_codes import StatusCode
 from tests.test_vera.data.urls import PetUrls
-from tests.test_vera.data.data_pets import get_pet_by_status
+from tests.test_vera.data.data_pets import get_pet_by_status, expected_non_exist_pet_keys
 from src.utils.http_methods import MyRequests
+from tests.test_vera.conf import pet_data
 
 
 class TestGetPet:
@@ -48,3 +49,39 @@ class TestGetPet:
         )
         assert response.status_code == self.status_code.OK, f"Unexpected status code."
         assert response.json() == []
+
+    def test_get_pet_by_id_status_code(self, pet_data):
+        """This test to get pet info by ID and ensures status code is 200"""
+        # creating a pet
+        response = MyRequests.post(
+            self.url.PET,
+            data=pet_data,
+        )
+        pet_id = response.json()['id']
+        # getting status code for the response
+        response = MyRequests.get(
+            self.url.PET + f"/{pet_id}",
+        )
+        pprint(response.json())
+        assert response.status_code == self.status_code.OK, f"Unexpected status code."
+
+    def test_get_pet_by_non_existing_id_status_code_404(self):
+        """This test to get pet info by non-existing ID and ensures status code is 404"""
+        pet_id = -1
+        response = MyRequests.get(
+            self.url.PET + f"/{pet_id}",
+        )
+        assert response.status_code == self.status_code.NOT_FOUND, \
+            f"Unexpected status code. Expected: {self.status_code.NOT_FOUND}, Actual: {response.status_code}"
+
+    @pytest.mark.parametrize("name, expected_value, error_message", expected_non_exist_pet_keys)
+    def test_get_pet_by_non_existing_id_error_message(self, name, expected_value, error_message):
+        """
+        This test validates error message in the response for retrieving pet information by non-existing ID
+        """
+        pet_id = -1
+        response = MyRequests.get(
+            self.url.PET + f"/{pet_id}",
+        )
+        assert name in response.json(), f"""response JSON doesn't have key '{name}'"""
+        assert response.json()[name] == expected_value, error_message
