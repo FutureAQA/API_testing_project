262b669033f3132e458bec60e035749701ade627
TatianaYurchenko <102853445+TatianaYurchenko@users.noreply.github.com>
2023-10-14 18:36:08 +0300

    feat: add 2 api-tests for put method (#26)

--- tests/test_tatiana/test_users/test_user_get.py
+++ tests/test_tatiana/test_users/test_user_get.py
@@ -7,7 +7,7 @@ class Test_users_get:
 
     def test_get_users_by_username_positive(self):
         url_post = f'{Urls.BASE_URL}{Urls.CREATE_USER}'
-        response_post = requests.post(url_post, json=data_user)
+        requests.post(url_post, json=data_user)
         url_get = f'{Urls.BASE_URL}{Urls.USER}{username}'
         response_get = requests.get(url_get)
         assert response_get.status_code == 200, "Wrong status code"
@@ -19,7 +19,7 @@ class Test_users_get:
         assert response.status_code == 404, "Wrong status code"
 
 
-    def test_get_users_by_empty_username(self):
+    def test_get_users_by_empty_username__negative(self):
         url_get = f'{Urls.BASE_URL}{Urls.USER}'
         response_get = requests.get(url_get)
         assert response_get.status_code == 405, "Wrong status code"
