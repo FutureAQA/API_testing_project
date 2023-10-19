b34acab7c689f4f8cb757a646b6ff223c85784cf
Ekaterina Zotova <ekatzotova@gmail.com>
2023-10-19 12:55:25 +0300

    fix: add mark.xfail to test_update_pet

--- tests/test_kate/test_get_pets.py
+++ tests/test_kate/test_get_pets.py
@@ -19,7 +19,6 @@ class TestsGetPets:
     @allure.title("test_get_pets_by_status")
     @pytest.mark.parametrize("status", AllData.PETS_STATUS)
     def test_get_pets_by_status(self, status):
-        # url = f'{AllData.URL_PET_STATUS}{status}'
         url = self.url.URL_PET_STATUS
         response = MyRequests().get(url, status)
         assert response.status_code == 200
