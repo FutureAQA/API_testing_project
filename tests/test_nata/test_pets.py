ebf99993abf38418e617a2b79a4f72e77e4c0bd7
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-10-20 02:45:30 +0400

    ref: test_pets.py

--- tests/test_nata/test_pets.py
+++ tests/test_nata/test_pets.py
@@ -1,7 +1,7 @@
 import requests
 import pytest
 import allure
-from tests.test_nata.constant_nata import PET_URL, FIND_BY_STATUS, UPLOAD_IMAGE, UPDATE_PET
+from tests.test_nata.constant_nata import PET_URL, FIND_BY_STATUS, UPDATE_PET
 from data.status_code import StatusCode
 
 
@@ -25,17 +25,21 @@ class TestPet:
     def test_get_pet_by_id(self, add_new_pet):
         url = f'{PET_URL}777'
         response = requests.get(url)
-        assert response.status_code == self.status_code.STATUS_OK, "Wrong status code"
+        assert response.status_code == self.status_code.STATUS_OK, \
+            f"Expected status {self.status_code.STATUS_OK}, actual status {response.status_code}"
 
+    @pytest.mark.xfail(reason="Undocumented status code")
     @allure.title("post_uploads_an_image_pet")
-    @pytest.mark.xfail
     def test_post_uploads_an_image_pet(self, add_new_pet):
         url = f'{PET_URL}777/uploadImage'
-        response = requests.post(url, json=UPLOAD_IMAGE)
-        assert response.status_code == self.status_code.STATUS_OK, "Wrong status code"
+        response = requests.post(url)
+        print(response.text)
+        assert response.status_code == self.status_code.STATUS_OK, \
+            f"Expected status {self.status_code.STATUS_OK}, actual status {response.status_code}"
 
     @allure.title('put_update_an_existing_pet')
     def test_put_update_an_existing_pet(self, add_new_pet):
         url = PET_URL
         response = requests.put(url, json=UPDATE_PET)
-        assert response.status_code == self.status_code.STATUS_OK, "Wrong status code"
+        assert response.status_code == self.status_code.STATUS_OK, \
+            f"Expected status {self.status_code.STATUS_OK}, actual status {response.status_code}"
