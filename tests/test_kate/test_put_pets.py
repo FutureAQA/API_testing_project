c5a4be2b974853db5a541b25e7a60f15101f79f0
Ekaterina Zotova <ekatzotova@gmail.com>
2023-10-24 20:16:17 +0300

    :

--- tests/test_kate/test_put_pets.py
+++ tests/test_kate/test_put_pets.py
@@ -13,6 +13,6 @@ class TestUpdatePets:
     @allure.title("test_update_pet")
     def test_update_pet(self):
         url = self.url.URL_PET
-        j = file_read('../../data/data_kate/body_update_pet.json')
+        j = file_read('data/data_kate/body_update_pet.json')
         response = MyRequests().put(url, data=j)
         assert response.status_code == 200, "Status_code is not 200"
